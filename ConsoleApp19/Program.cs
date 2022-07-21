5bc7c59525e1d45ad047958b396ae5faf8caaf6a
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-21 17:02:08 +0300

    Add files via upload

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1,28);
+            int randomMinNumber = 100;
+            int randomMaxNumber = 999;
+
+            Console.WriteLine("Случайное число: " + randomNumber);
+            Console.ReadLine();
+
+            for (int i = randomMinNumber; i <= randomMaxNumber; i += randomNumber)
+            {
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+        }
+    }
+}
